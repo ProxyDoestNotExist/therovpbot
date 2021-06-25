af873d88d0c92c2f0739abbd5e93b65dac44f1fe
Fazil vk <76457800+fazilvk786@users.noreply.github.com>
2021-06-25 18:44:42 +0530

    Update del.js

--- plugins/del.js
+++ plugins/del.js
@@ -2,7 +2,7 @@ const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 
 
-Asena.addCommand({pattern: 'del', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'del', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 
   await message.reply_message.delete();
 
