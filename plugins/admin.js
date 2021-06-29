3765c196ce60bff7e696be1f58d3ef55e5fe814d
Fazil vk <76457800+fazilvk786@users.noreply.github.com>
2021-06-29 17:50:49 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -1,5 +1,6 @@
-const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
+const {MessageType, GroupSettingChange, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const Asena = require('../events');
+const fs = require('fs');
 const Config = require('../config');
 
 const Language = require('../language');
