49b025c539a35ce9143fc6ce88e50a7eb964653a
ProxyDoestNotExist <86024494+ProxyDoestNotExist@users.noreply.github.com>
2021-08-12 20:59:51 +0530

    Update txttoimg.js

--- plugins/txttoimg.js
+++ plugins/txttoimg.js
@@ -7,7 +7,7 @@ Github: https://github.com/phaticusthiccy/TextMaker-Unlimited
 
 This code works with unlimited and completely free an API scraper.
 Don't use it for illegal purposes.
-*/
+
 
 const thiccysapi = require('textmaker-thiccy'); // Import NPM Package
 
@@ -761,3 +761,4 @@ Asena.addCommand({pattern: 'cup ?(.*)', fromMe: wk, dontAddCommandList: true}, (
           } 
     });
 }));
+*/
