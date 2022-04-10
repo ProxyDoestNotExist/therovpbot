e34ad113b9054e6835ab651403f2aef073e007fc
ProxyDoesNotExist <102735798+ProxyDoesNotExist@users.noreply.github.com>
2022-04-10 11:50:18 +0530

    Update someonesay.js

--- plugins/someonesay.js
+++ plugins/someonesay.js
@@ -64,7 +64,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'animesay ?(.*)', fromMe: false, desc: Lang.ANIME_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'kannasay ?(.*)', fromMe: false, desc: Lang.ANIME_DESC }, (async (message, match) => {
 
         if (message.jid === '905524317852-1612300121@g.us') {
 
